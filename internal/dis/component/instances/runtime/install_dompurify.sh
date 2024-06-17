4414acbc702b694150a6348d2c28374eb9297f7b
rnsrk <robert@nasarek.org>
2024-06-17 17:02:04 +0200

    adjust dompurify lib install opts

--- internal/dis/component/instances/runtime/install_dompurify.sh
+++ internal/dis/component/instances/runtime/install_dompurify.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "=> Creating 'sites/default/libraries/dompurity/dist/' directory"
-mkdir -p /var/www/data/project/web/libraries/dompurity/dist/
+mkdir -p /var/www/data/project/web/libraries/dompurify/dist/
 
 echo "=> Downloading 'purify.min.js' and 'LICENSE'"
-curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/dist/purify.min.js -o /var/www/data/project/web/libraries/DOMPurify/dist/purify.min.js
-curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/LICENSE -o /var/www/data/project/web/libraries/DOMPurify/LICENSE
+curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/dist/purify.min.js -o /var/www/data/project/web/libraries/dompurify/dist/purify.min.js
+curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/LICENSE -o /var/www/data/project/web/libraries/dompurify/LICENSE
 
 echo "=> Done"
