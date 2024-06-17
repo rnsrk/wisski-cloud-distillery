bb697d7b5d336b825b0c6627bfc6fb27a0f3cb8e
rnsrk <robert@nasarek.org>
2024-06-17 17:01:45 +0200

    adjust colorbox lib install opts

--- internal/dis/component/instances/runtime/install_colorbox.sh
+++ internal/dis/component/instances/runtime/install_colorbox.sh
@@ -6,10 +6,10 @@ chmod 777 /var/www/data/project/web/sites/default/
 trap "chmod 755 /var/www/data/project/web/sites/default/" EXIT
 
 echo "=> Creating 'sites/default/libraries/colorbox/' directory"
-mkdir -p /var/www/data/project/web/sites/default/libraries/colorbox
+mkdir -p /var/www/data/project/web/libraries/colorbox
 
 echo "=> Downloading 'jquery.colorbox-min.js' and 'LICENSE.md'"
-curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/LICENSE.md -o /var/www/data/project/web/sites/default/libraries/colorbox/LICENSE.md
-curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/jquery.colorbox-min.js -o /var/www/data/project/web/sites/default/libraries/colorbox/jquery.colorbox-min.js
+curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/LICENSE.md -o /var/www/data/project/web/libraries/colorbox/LICENSE.md
+curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/jquery.colorbox-min.js -o /var/www/data/project/web/libraries/colorbox/jquery.colorbox-min.js
 
 echo "=> Done"
