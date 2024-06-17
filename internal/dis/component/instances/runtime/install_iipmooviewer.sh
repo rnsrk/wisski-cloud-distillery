c263efee7a80f50043d654b49afd9a2d499a7f42
rnsrk <robert@nasarek.org>
2024-06-17 17:02:19 +0200

    adjust iipmooserver lib install opts

--- internal/dis/component/instances/runtime/install_iipmooviewer.sh
+++ internal/dis/component/instances/runtime/install_iipmooviewer.sh
@@ -5,13 +5,12 @@ echo "=> Setting up filesystem permissions"
 chmod 777 /var/www/data/project/web/sites/default/
 trap "chmod 755 /var/www/data/project/web/sites/default/" EXIT
 
-echo "=> Download 'IIPMooViewer' repo"
-wget https://github.com/ruven/iipmooviewer/archive/refs/heads/master.zip -P /var/www/data/project/web/sites/default/libraries/
-echo "=> Unzip 'IIPMooViewer' repo"
-unzip /var/www/data/project/web/sites/default/libraries/master.zip -d web/libraries/
-echo "=> Remove 'IIPMooViewer' zipped package"
-rm -r /var/www/data/project/web/sites/default/libraries/master.zip
-echo "=> Rename 'IIPMooViewer' library"
-mv /var/www/data/project/web/sites/default/libraries/iipmooviewer-master web/libraries/iipmooviewer
+echo "=> Creating 'sites/default/libraries/iipmooviewer' directory"
+mkdir -p /var/www/data/project/web/libraries/iipmooviewer
+
+echo "=> Download 'IIPMooViewer'"
+curl -L https://raw.githubusercontent.com/ruven/iipmooviewer/master/js/iipmooviewer-2.0-min.js -o /var/www/data/project/web/libraries/iipmooviewer/iipmooviewer-2.0-min.js
 
 echo "=> Done"
+
+
