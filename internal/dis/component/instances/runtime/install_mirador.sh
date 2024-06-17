47b53ba4f5682d1a19d2f2f1172328063f8c5994
rnsrk <robert@nasarek.org>
2024-06-17 17:03:13 +0200

    adjust mirador lib install opts

--- internal/dis/component/instances/runtime/install_mirador.sh
+++ internal/dis/component/instances/runtime/install_mirador.sh
@@ -6,9 +6,9 @@ chmod 777 /var/www/data/project/web/sites/default/
 trap "chmod 755 /var/www/data/project/web/sites/default/" EXIT
 
 echo "=> Creating 'sites/default/libraries/wisski-mirador-integration/' directory"
-mkdir -p /var/www/data/project/web/sites/default/libraries/wisski-mirador-integration
+mkdir -p /var/www/data/project/web/libraries/wisski-mirador-integration
 
 echo "=> Downloading 'mirador-integration.js'"
-curl -L https://raw.githubusercontent.com/rnsrk/wisski-mirador-integration/main/mirador-integration.js -o /var/www/data/project/web/sites/default/libraries/wisski-mirador-integration/mirador-integration.js
+curl -L https://raw.githubusercontent.com/rnsrk/wisski-mirador-integration/main/mirador-integration.js -o /var/www/data/project/web/libraries/wisski-mirador-integration/mirador-integration.js
 
 echo "=> Done"
